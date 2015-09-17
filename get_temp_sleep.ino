3942e52dcf56a6d73f3ea6d4fd6b396809056b56
Gerasimos Kounadis <gerrykoun@gmail.com>
2015-09-17 19:23:43 +0300

    temperature and sleeping example

---
+++ get_temp_sleep.ino
@@ -0,0 +1,44 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LowPower.h>
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+ 
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+ 
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+ int wait=0;
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  // Start up the library
+  sensors.begin();
+}
+ 
+void loop(void) {
+	
+Serial.print("wait =");
+Serial.println(wait);
+  if(wait>=8){
+     // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  Serial.print(" Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+
+  Serial.print("Temperature for Device 1 is: ");
+  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"? 
+    // You can have more than one IC on the same bus. 
+    // 0 refers to the first IC on the wire
+    wait=0;
+    }	  	  
+    delay(100);//doulepse ok me delay 1000 kai 500
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);	  
+  wait++;
+  //delay(500);
+}
